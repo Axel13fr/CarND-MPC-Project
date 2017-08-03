417b1fb571830037fe728ab2dcd14b3a28707561
Unknown <axel.durbec@gmail.com>
2017-08-03 13:04:12 +0200

    NEW: initial implementation
    World to local coordinate convertion OK, MPC NOK

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,38 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+struct track{
+    std::vector<double> x;
+    std::vector<double> y;
+};
+
+/**
+ * @brief ConvertPtsToLocalTrack
+ * @param in the points making the input waypoints in global coordinate system
+ * @param x current veh pos
+ * @param y current veh po
+ * @param psi current veh orientation
+ * @return waypoints converted to vehicle coordinate system
+ */
+track ConvertPtsToLocalTrack(const track& in,double x,double y,double psi){
+    track veh_coord_wp;
+
+    Eigen::MatrixXd rot(2,2);
+    rot << cos(-psi), -sin(-psi),
+            sin(-psi), cos(-psi);
+    Eigen::VectorXd veh(2);
+    veh << x,y;
+    for(size_t i = 0; i < in.x.size(); i++){
+        Eigen::VectorXd wp(2);
+        wp << in.x[i] , in.y[i];
+        Eigen::VectorXd wp_veh_coord = rot*(wp - veh);
+        veh_coord_wp.x.push_back(wp_veh_coord[0]);
+        veh_coord_wp.y.push_back(wp_veh_coord[1]);
+    }
+
+    return veh_coord_wp;
+}
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -39,6 +71,14 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
     result += coeffs[i] * pow(x, i);
   }
   return result;
+}
+// Evaluate the derivative of a polynomial
+double polyDerivEval(Eigen::VectorXd coeffs, double x) {
+    double result = 0.0;
+    for (int i = 1; i < coeffs.size(); i++) {
+      result += coeffs[i] * pow(x, i-1) * i;
+    }
+    return result;
 }
 
 // Fit a polynomial.
@@ -85,6 +125,7 @@ int main() {
         string event = j[0].get<string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
+          // All points here are given in world coordinate system
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
           double px = j[1]["x"];
@@ -93,23 +134,38 @@ int main() {
           double v = j[1]["speed"];
 
           /*
-          * TODO: Calculate steering angle and throttle using MPC.
+          * Calculate steering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+           // Fit a polynomial to the above x and y coordinates (path to follow)
+          Eigen::VectorXd ptx = Eigen::Map<Eigen::VectorXd>(ptsx.data(),ptsx.size());
+          Eigen::VectorXd pty = Eigen::Map<Eigen::VectorXd>(ptsy.data(),ptsy.size());
+          auto coeffs = polyfit(ptx,pty,3) ;
+
+          // calculate the cross track error
+          auto y_out = polyeval(coeffs,px);
+          double cte = y_out - py;
+          // calculate the orientation error
+          auto poly_deriv = polyDerivEval(coeffs,px);
+          double epsi = psi - atan(poly_deriv);
+
+          Eigen::VectorXd state(6);
+          state << px, py, psi, v, cte, epsi;
+          auto vars = mpc.Solve(state,coeffs);
+          double steer_value(vars[6]);
+          double throttle_value(vars[7]);
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
+          msgJson["steering_angle"] = steer_value / deg2rad(25);
           msgJson["throttle"] = throttle_value;
 
           //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+          vector<double> mpc_x_vals{vars[0],vars[2],vars[4]};
+          vector<double> mpc_y_vals{vars[1],vars[3],vars[5]};
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
@@ -118,14 +174,16 @@ int main() {
           msgJson["mpc_y"] = mpc_y_vals;
 
           //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
+          track wp{ptsx,ptsy};
+          auto ref_track = ConvertPtsToLocalTrack(wp,px,py,psi);
+          //vector<double> next_x_vals{1,2,3,4,5,6,7,8,9,10};
+          //vector<double> next_y_vals{0,0,0,0,0,0,0,0,0,0};
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
-          msgJson["next_x"] = next_x_vals;
-          msgJson["next_y"] = next_y_vals;
+          msgJson["next_x"] = ref_track.x;
+          msgJson["next_y"] = ref_track.y;
 
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
@@ -139,7 +197,7 @@ int main() {
           //
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
           // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(100));
+          this_thread::sleep_for(chrono::milliseconds(0));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
